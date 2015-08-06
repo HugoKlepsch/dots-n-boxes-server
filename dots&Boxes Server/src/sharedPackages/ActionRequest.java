a6e2c6289940f21964880f3813c76c89844399b9
Graham <ghildebrandt@outlook.com>
2015-08-06 15:52:50 -0400

    added some more action types

--- dots&Boxes Server/src/sharedPackages/ActionRequest.java
+++ dots&Boxes Server/src/sharedPackages/ActionRequest.java
@@ -18,6 +18,21 @@ public class ActionRequest implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	public static final int CS_CONNECT = 0;
+	public static final int CS_DISCONNECT = 1;
+	//ask to join game
+	public static final int CS_JOIN_GAME = 2;
+	//make the user join the game
+	public static final int SC_JOIN_GAME = 3;
+	//send the request for matchmaking
+	public static final int CS_FIND_GAME = 4;
+	//challenge player
+	public static final int CS_CHALLENGE_PLAYER = 5;
+	//receive challenge
+	public static final int SC_CHALLENGE_PLAYER = 6;
+	//ask for user list
+	public static final int CS_USERLIST = 7;
+	//send user list
+	public static final int SC_USERLIST = 8;
 	
 	private int action;
 	private User user;
