d3afc585b08b4d52b7544a61fc068df3b9bef78b
Hugo Klepsch <hugo.klepsch@gmail.com>
2015-08-06 13:16:06 -0400

    Added player class

---
+++ dots&Boxes Server/src/server/Plaer.java
@@ -0,0 +1,17 @@
+/*
+		 Title: Plaer.java
+		 Programmer: hugo
+		 Date of creation: Aug 6, 2015
+		 Description: 
+*/
+
+
+package server;
+
+/**
+ * @author hugo
+ *
+ */
+public class Plaer {
+
+}
