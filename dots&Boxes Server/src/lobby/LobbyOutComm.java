86197872dcf94314ab2d39bac74fba6beba02078
Graham <ghildebrandt@outlook.com>
2015-08-06 14:29:30 -0400

    added some logging messages to in and out comms

--- dots&Boxes Server/src/lobby/LobbyOutComm.java
+++ dots&Boxes Server/src/lobby/LobbyOutComm.java
@@ -12,6 +12,8 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+import server.Logging;
+
 /**
  * @author hugo
  *
@@ -35,6 +37,7 @@ public class LobbyOutComm extends Thread{
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			Lobby.logger.writeToLog("Could not find output stream of socket");
 		}
 	}
 
